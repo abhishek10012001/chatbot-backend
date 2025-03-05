f67570718f512c70b6ce23b9420cc9f36ec7f9f3
abhishek10012001 <kumarabhishek10012001@gmail.com>
2025-03-05 15:20:07 +0530

    patch: added logging and basic delete message api

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { getRuntimeEnv, RuntimeEnv } from "./runtime_env";
 import * as admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
+import { Logger } from "@firebase/logger";
 
 const runtimeEnv: RuntimeEnv = getRuntimeEnv();
 console.log(`RuntimeEnv: ${runtimeEnv}`);
@@ -25,10 +26,12 @@ const getBotResponse = (userText: string) => {
 };
 
 app.post("/api/v1/sendMessage", async (req: Request, res: Response): Promise<void> => {
+  const logger = new Logger("sendMessage");
     try {
       const { text, userId } = req.body;
   
       if (!text || !userId) {
+        logger.error("Either of userId, text is missing")
         res.status(400).json({ error: "UserId and text are missing" });
         return;
       }
@@ -42,13 +45,60 @@ app.post("/api/v1/sendMessage", async (req: Request, res: Response): Promise<voi
         [timestamp]: userMessage,
         [Date.now() + 1000]: botResponse,
       }, { merge: true });
+
+      logger.info(`Successfully replied and saved user message for userId: ${userId}`);
   
       res.status(201).json({ message: "Message saved", id: userId});
     } catch (error) {
       console.error("Error saving message:", error);
       res.status(500).json({ error: "Internal Server Error" });
     }
-  });
+});
+
+app.delete("/api/v1/deleteMessage", async (req: Request, res: Response): Promise<void> =>  {
+  const logger = new Logger("deleteMessage");
+  const { userId, messageId } = req.body;
+  
+  if (!userId || !messageId) {
+    logger.error("Either of userId, text is missing")
+    res.status(400).json({ error: "UserId and messageId are missing" });
+    return;
+  }
+
+  const userRef = db.collection("Messages").doc(userId);
+  const userDoc = await userRef.get();
+  
+  if (!userDoc.exists) {
+    logger.error(`User doc doesn't exist for userId:${userId}`);
+    res.status(404).json({ error: "User not found" });
+    return;
+  }
+
+  const messages: admin.firestore.DocumentData | undefined = userDoc.data();
+
+  if(!messages) {
+    logger.error(`No message exists for userId: ${userId}`);
+    res.status(404).json({ error: "No messages exist for this user" });
+    return;
+  }
+
+  if (!messages[messageId]){
+    logger.error(`No message exists for messageId:${messageId}, userId: ${userId}`);
+    res.status(403).json({ error: "No message exists with this messageId" });
+    return;
+  }
+  
+  if (messages[messageId].by !== "user") {
+    logger.error(`Non user message tried to be deleted by user for messageId:${messageId}, userId: ${userId}. Please investigate`);
+    res.status(403).json({ error: "Cannot delete this message" });
+    return;
+  }
+
+  logger.info(`Successfully deleted user message for messageId: ${messageId}, userId: ${userId}`);
+
+  await userRef.update({ [messageId]: admin.firestore.FieldValue.delete() });
+  res.json({ success: true, message: "Message deleted" });
+});
 
 const PORT: number = parseInt(process.env.PORT || "5001", 10);
 app.listen(PORT, () => {
