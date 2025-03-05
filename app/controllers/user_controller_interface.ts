af0fe17d5101b1dc4a440993ccdc301083ca5e09
abhishek10012001 <kumarabhishek10012001@gmail.com>
2025-03-05 22:18:49 +0530

    refactor: corrected indentation

--- app/controllers/user_controller_interface.ts
+++ app/controllers/user_controller_interface.ts
@@ -7,12 +7,12 @@ export interface ChatbotControllerInterface {
     ): Promise<Response<unknown, Record<string, unknown>>>;
 
     editMessage(
-        req: Request,
-        resp: Response,
+      req: Request,
+      resp: Response,
     ): Promise<Response<unknown, Record<string, unknown>>>;
 
     deleteMessage(
-    req: Request,
-    resp: Response,
+      req: Request,
+      resp: Response,
     ): Promise<Response<unknown, Record<string, unknown>>>;
   }
