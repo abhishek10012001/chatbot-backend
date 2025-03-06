5bd6c2d4a13fcba170ce74b032b0f5038f4bb369
abhishek10012001 <kumarabhishek10012001@gmail.com>
2025-03-06 20:19:55 +0530

    patch: added test case for edit api

--- app/tests/chatbot.test.ts
+++ app/tests/chatbot.test.ts
@@ -5,7 +5,13 @@ import { StatusCode } from "../enums/auth_status_code";
 describe("Chatbot API - sendMessage", () => {
   const validApiKey = process.env.API_SECRET_KEY!;
   const userId = "test-user-123";
+  const messageId: string = "1741269454219";
 
+  /**
+   * The below test cases belong to sendMessage API
+   */
+
+  // When Invalid API key is used to call the API
   it("Should return 401 is invalid API key", async () => {
     const response = await request(expressApp)
       .post("/api/v1/sendMessage")
@@ -15,6 +21,7 @@ describe("Chatbot API - sendMessage", () => {
     expect(response.body).toHaveProperty("code", StatusCode.INVALID_API_KEY);
   });
 
+  // When all required parameters are not passed
   it("Should return 400 if missing required param", async () => {
     const response = await request(expressApp)
       .post("/api/v1/sendMessage")
@@ -25,6 +32,7 @@ describe("Chatbot API - sendMessage", () => {
     expect(response.body).toHaveProperty("code", StatusCode.MISSING_REQUIRED_PARAMETERS);
   });
 
+  // When all conditions are met to call the API
   it("Should return 200 and a bot response when sending a valid message", async () => {
     const response = await request(expressApp)
       .post("/api/v1/sendMessage")
@@ -35,6 +43,67 @@ describe("Chatbot API - sendMessage", () => {
     expect(response.body).toHaveProperty("code", StatusCode.SUCCESS);
     expect(response.body).toHaveProperty("botResponseId");
     expect(response.body).toHaveProperty("userMessageId");
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toBeTruthy();
+  });
+
+  /**
+   * The below test cases belong to editMessage API
+   */
+
+  // When Invalid API key is used to call the API
+  it("Should return 401 is invalid API key", async () => {
+    const response = await request(expressApp)
+      .post("/api/v1/editMessage")
+      .send({ newText: "Hello!", userId: userId, messageId: messageId });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty("code", StatusCode.INVALID_API_KEY);
+  });
+
+  // When all required parameters are not passed
+  it("Should return 400 if missing required param", async () => {
+    const response = await request(expressApp)
+      .post("/api/v1/editMessage")
+      .set("x-api-key", validApiKey)
+      .send({ newText: "" });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("code", StatusCode.MISSING_REQUIRED_PARAMETERS);
+  });
+
+  // When invalid user id is passed
+  it("Should return 404 if invalid user id is passed", async () => {
+    const response = await request(expressApp)
+      .post("/api/v1/editMessage")
+      .set("x-api-key", validApiKey)
+      .send({ newText: "a",userId: "b", messageId: "c" });
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty("code", StatusCode.USER_NOT_FOUND);
+  });
+
+  // When invalid message id is passed
+  it("Should return 403 if invalid message id is passed", async () => {
+    const response = await request(expressApp)
+      .post("/api/v1/editMessage")
+      .set("x-api-key", validApiKey)
+      .send({ newText: "a",userId: userId, messageId: "c" });
+
+    expect(response.status).toBe(403);
+    expect(response.body).toHaveProperty("code", StatusCode.MESSAGE_NOT_FOUND);
+  });
+
+  // When all conditions are met to call the API
+  it("Should return 200 and a bot response when sending a valid message", async () => {
+    const response = await request(expressApp)
+      .post("/api/v1/editMessage")
+      .set("x-api-key", validApiKey)
+      .send({ newText: "Hello!", userId: userId, messageId: messageId });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("code", StatusCode.SUCCESS);
+    expect(response.body).toHaveProperty("botResponseId");
     expect(response.body).toHaveProperty("message");
     expect(response.body.message).toBeTruthy();
   });
